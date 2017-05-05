110f065c2b2ed474cfdf06fc8e28a9671728c63b
Shakti <shaktirp1@gmail.com>
2017-05-04 17:23:50 -0700

    updated project page design, added flickr

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,23 +1,49 @@
-console.log('i am main');
+// console.log('i am main');
+//
+// var feed = new Instafeed({
+//   get:'user',
+// 	userId: 448158681,
+//   resolution: 'standard_resolution',
+// 	accessToken: '448158681.6eb286a.983539d159db48fb8f9ca6c8942f327d',
+//   after: function () {
+//     var images = $("#instafeed").find('a');
+//     $.each(images, function(index, image) {
+//       var delay = (index * 75) + 'ms';
+//       $(image).css('-webkit-animation-delay', delay);
+//       $(image).css('-moz-animation-delay', delay);
+//       $(image).css('-ms-animation-delay', delay);
+//       $(image).css('-o-animation-delay', delay);
+//       $(image).css('animation-delay', delay);
+//       $(image).addClass('animated flipInX');
+//     });
+//   },
+//   template: '<div><img src="{{image}}" /></div>'
+// });
+//
+// feed.run();
 
-var feed = new Instafeed({
-  get:'user',
-	userId: 448158681,
-  resolution: 'standard_resolution',
-	accessToken: '448158681.6eb286a.983539d159db48fb8f9ca6c8942f327d',
-  after: function () {
-    var images = $("#instafeed").find('a');
-    $.each(images, function(index, image) {
-      var delay = (index * 75) + 'ms';
-      $(image).css('-webkit-animation-delay', delay);
-      $(image).css('-moz-animation-delay', delay);
-      $(image).css('-ms-animation-delay', delay);
-      $(image).css('-o-animation-delay', delay);
-      $(image).css('animation-delay', delay);
-      $(image).addClass('animated flipInX');
-    });
-  },
-  template: '<div><img src="{{image}}" /></div>'
-});
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=391bbc9206d6d6250875ff51556a0f5d&format=json&user_id=143684909@N08",
+  "method": "GET",
+  // "format": "json",
+  "headers": {}
+}
 
-feed.run();
+$.ajax(settings).done(function(data) {
+  eval(data)
+}).fail(function(data) {
+  console.log('error', data)
+})
+
+function jsonFlickrApi(json) {
+  console.log(json);
+
+  $.each(json.photos.photo, function(i, item) {
+    var src = "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg"
+    console.log(src);
+
+    $("<div>").append($("<img />").attr("src", src)).appendTo("#instafeed")
+  });
+};
